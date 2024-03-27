83725ac3fdacd06f9636750de20b5235aae05f84
GearMonte <73728470+GearMonte@users.noreply.github.com>
2024-03-26 23:08:10 -0500

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,8 +1,7 @@
 from functions import json_df 
-# import demo as d
-# This references the file a
-# function called json_df is 
-# saved.
+# This references the file functions.py
+# where the function called json_df
+# is saved.
 
 df = json_df('data.json')
 print(df)
